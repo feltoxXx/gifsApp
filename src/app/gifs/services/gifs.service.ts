0a0dd3892ae3b6ce478b8b62277142600959529e
feltoxXx <luislagardera@gmail.com>
2022-07-11 19:26:32 -0300

    Estamos en el punto del uso de LocalStorage

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey    : string = 'c9DIu0sv0dVmw8Fxg6bRK5hCx6oqG4oI';
+
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    
+    return [...this._historial];
+  }
+
+  constructor ( private http: HttpClient) {
+    
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+    // if ( localStorage.getItem('historial')) {
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );
+    // }
+
+  }
+
+  buscarGifs( query: string ) {
+
+    query = query.trim().toLowerCase();
+
+    if ( !this._historial.includes( query ) ) {
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ))
+    }
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${ this.apiKey }&q=${ query }&limit=10`)
+      .subscribe( ( resp ) => {
+        console.log( resp.data );
+        this.resultados = resp.data
+      });
+
+  }
+
+}
